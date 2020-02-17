ee9198d936a1add44d50b8b4127b567f4f7ca35f
rey242 <41313374+rey242@users.noreply.github.com>
2020-02-16 22:27:30 -0600

    Continued Development with Email
    Continued dev.

--- email.py
+++ email.py
@@ -1,9 +1,11 @@
+import re
 
 class Email:
     def __init__(self):
         self.address = None
         self.__password__ = None
         self.port = None
+        self.smtp = None
 
     def set_address(self,email):
         """ Set an email address to an account"""
@@ -16,4 +18,31 @@ class Email:
     def set_port(self,port):
         """ Set email port to be used for account"""
         self.port = port
+
+    def set_smtp_server(self,address):
+        self.smtp = address
     
+    def check_email_format(self,email):
+        """ Check if email is in right format """
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            raise Exception("Invalid email format")
+        else:
+            return "Email format is ok"
+
+class Message:
+    def __init__(self):
+        self.receiver = None
+
+    def set_receiver(self,email):
+        """ Specify receipent of the email message """
+        self.receiver = email
+    
+    def send_text_email(self,email,msg):
+        """ Send a Plain-text email using SMTP_SSL """
+        if msg == '1':
+            raise Exception()
+
+msg = Message()
+msg.send_text_email(Email(),'hi')
+
+
--- email_test.py
+++ email_test.py
@@ -14,6 +14,29 @@ class EmailTest(unittest.TestCase):
         an_email = Email()
         an_email.set_port(444)
         self.assertEqual(an_email.port,444)
+    def test_smtp(self):
+        an_email = Email()
+        an_email.set_smtp_server('smtp.gmail.com')
+        self.assertEqual(an_email.smtp,'smtp.gmail.com')
+    def test_email_format(self):
+        an_email = Email()
+        with self.assertRaises(Exception):
+            an_email.check_email_format("email.com")
+    def test_sender_email(self):
+        msg = Message()
+        msg.set_receiver('them@theiremail.com')
+        self.assertEqual(msg.receiver,'them@theiremail.com')
+    def test_send_text_email(self):
+        an_email = Email()
+        an_email.set_address('test.email@email.com')
+        an_email.set_password('mypass')
+        an_email.set_port(444)
+        msg = Message()
+        msg.set_receiver('email@email.com')
+        my_message = "1"
+        with self.assertRaises(Exception):
+            msg.send_text_email(an_email, my_message)
+
 
 if __name__ == '__main__':
     unittest.main()
