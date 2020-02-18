44644f607e2252a3dac1fe3beb9250954a13d8a6
Reynold Guzman <rey242@gmail.com>
2020-02-17 22:29:18 -0600

    FInal Email - Text
    Final Code for sending text email.

--- app_email.py
+++ app_email.py
@@ -1,6 +1,6 @@
-import re
 import smtplib
 import ssl
+
 
 class Email:
     def __init__(self):
@@ -10,45 +10,50 @@ class Email:
         self.smtp = None
         self.username = None
         self.email_name = None
-    def set_address(self,email):
+
+    def set_address(self, email):
         """ Set an email address to an account"""
         self.address = email
-    def set_password(self,password):
+
+    def set_password(self, password):
         """ Set a password to an account."""
         self.password = password
-    def set_port(self,port):
+
+    def set_port(self, port):
         """ Set email port to be used for account"""
         self.port = port
-    def set_smtp_server(self,address):
+
+    def set_smtp_server(self, address):
         """ Set email server to be used for account"""
         self.smtp = address
-    def set_username(self,username):
+
+    def set_username(self, username):
         """ Set username for account, maybe blank"""
         self.username = username
-    def set_email_name(self,name):
+
+    def set_email_name(self, name):
         """ Set Name to be sent from"""
         self.email_name = name
-    
-    def check_email_format(self,email):
-        """ Check if email is in right format """
-        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-            raise Exception("Invalid email format")
-        else:
-            return "Email format is ok"
+
 
 class Message:
     def __init__(self):
         self.receiver = None
-    def set_receiver(self,email):
+
+    def set_receiver(self, email):
         """ Specify receipent of the email message """
         self.receiver = email
-    def send_text_email(self,email_account,message):
+
+    def send_text_email(self, email_account, message):
         with smtplib.SMTP(email_account.smtp, email_account.port) as server:
             server.login(email_account.username, email_account.password)
             server.sendmail(email_account.address, self.receiver, message)
-    def send_ssl_text_email(self,email_account,message):
+        return "Message Sent"
+
+    def send_ssl_text_email(self, email_account, message):
         context = ssl.create_default_context()
         with smtplib.SMTP(email_account.smtp, email_account.port) as server:
             server.starttls(context=context)
             server.login(email_account.username, email_account.password)
             server.sendmail(email_account.address, self.receiver, message)
+        return "Message Sent"
--- email_test.py
+++ email_test.py
@@ -1,41 +1,74 @@
 import unittest
-from email import *
+from app_email import Email, Message
+
 
 class EmailTest(unittest.TestCase):
     def test_email_address(self):
         an_email = Email()
         an_email.set_address('test.email@email.com')
-        self.assertEqual(an_email.address,'test.email@email.com')
+        self.assertEqual(an_email.address, 'test.email@email.com')
+
     def test_password(self):
         an_email = Email()
         an_email.set_password('mypass')
-        self.assertEqual(an_email.__password__ , 'mypass')
+        self.assertEqual(an_email.password, 'mypass')
+
     def test_port(self):
         an_email = Email()
         an_email.set_port(444)
-        self.assertEqual(an_email.port,444)
+        self.assertEqual(an_email.port, 444)
+
     def test_smtp(self):
         an_email = Email()
         an_email.set_smtp_server('smtp.gmail.com')
-        self.assertEqual(an_email.smtp,'smtp.gmail.com')
-    def test_email_format(self):
-        an_email = Email()
-        with self.assertRaises(Exception):
-            an_email.check_email_format("email.com")
+        self.assertEqual(an_email.smtp, 'smtp.gmail.com')
+
     def test_sender_email(self):
         msg = Message()
         msg.set_receiver('them@theiremail.com')
-        self.assertEqual(msg.receiver,'them@theiremail.com')
+        self.assertEqual(msg.receiver, 'them@theiremail.com')
+
     def test_send_text_email(self):
-        an_email = Email()
-        an_email.set_address('test.email@email.com')
-        an_email.set_password('mypass')
-        an_email.set_port(444)
+        em = Email()
         msg = Message()
-        msg.set_receiver('email@email.com')
-        my_message = "1"
-        with self.assertRaises(Exception):
-            msg.send_text_email(an_email, my_message)
+
+        msg.set_receiver("to@smtp.mailtrap.io")
+        em.set_address("from@smtp.mailtrap.io")
+        em.set_smtp_server("smtp.mailtrap.io")
+        em.set_port(2525)
+        em.set_username("c123d2a294f857")
+        em.set_password("07dc82c500f623")
+        em.set_email_name('A user')
+        
+        message = f"""\
+        Subject: Hi Mailtrap - class
+        To: {msg.receiver}
+        From: {em.email_name} <{em.address}>
+            
+        This is a test e-mail message from class."""
+        
+        self.assertEqual(msg.send_text_email(em, message),"Message Sent")
+    
+    def test_send_ssl_text_email(self):
+        em = Email()
+        msg = Message()
+
+        msg.set_receiver("to@smtp.mailtrap.io")
+        em.set_address("from@smtp.mailtrap.io")
+        em.set_smtp_server("smtp.mailtrap.io")
+        em.set_port(2525)
+        em.set_username("c123d2a294f857")
+        em.set_password("07dc82c500f623")
+        em.set_email_name('A user')
+        
+        message = f"""\
+        Subject: Hi Mailtrap - SSL email
+        To: {msg.receiver}
+        From: {em.email_name} <{em.address}>
+            
+        This is a test e-mail message with SSL."""
+        
+        self.assertEqual(msg.send_ssl_text_email(em, message),"Message Sent")
 
 
 if __name__ == '__main__':
