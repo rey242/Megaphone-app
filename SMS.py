b1f0e5a60c42feee098c1624b4850e88d5c38bda
gerrodgomez <60899850+gerrodgomez@users.noreply.github.com>
2020-02-24 20:42:50 -0600

    Add files via upload

---
+++ SMS.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29806.167
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "SMS.py", "..\..\..\source\repos\Software agile\MMS code\MMS code\SMS.py.pyproj", "{194A937F-C007-4305-ABBA-A87E82A652DC}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{194A937F-C007-4305-ABBA-A87E82A652DC}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{194A937F-C007-4305-ABBA-A87E82A652DC}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {4000905A-3231-48D8-8FBA-91A618605278}
+	EndGlobalSection
+EndGlobal
