a49677a50e9d18a485db041a6f31d5de94fd1565
gerrodgomez <60899850+gerrodgomez@users.noreply.github.com>
2020-02-26 11:58:35 -0600

    Update Outbound to phone by MMS Code.py
    this has the actual SID and Token and not the environment variables. I would like to make the SID and Token no visible in the code but no need to worry!

--- Outbound to phone by MMS Code.py
+++ Outbound to phone by MMS Code.py
@@ -1,8 +1,8 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+account_sid = os.environ["AC305c6618726da7bb98419806610db628"]
+auth_token = os.environ["6461f40481de49a44c7d5875c3ee7468"]
 
 client = Client(account_sid, auth_token)
 
